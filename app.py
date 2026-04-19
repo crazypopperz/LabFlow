c037c71b82f948104505ae7c74fe54b32a095934
crazypopperz <hazardouskill@gmail.com>
2026-04-19 19:38:08 +0200

    fix: switch Flask-Mail to SendGrid SMTP

--- app.py
+++ app.py
@@ -112,12 +112,12 @@ def create_app():
     limiter.init_app(app)
     cache.init_app(app)
 
-    # Flask-Mail config
-    app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+    # Flask-Mail config via SendGrid
+    app.config['MAIL_SERVER'] = 'smtp.sendgrid.net'
     app.config['MAIL_PORT'] = 587
     app.config['MAIL_USE_TLS'] = True
-    app.config['MAIL_USERNAME'] = 'help.scientral@gmail.com'
-    app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
+    app.config['MAIL_USERNAME'] = 'apikey'
+    app.config['MAIL_PASSWORD'] = os.environ.get('SENDGRID_API_KEY')
     app.config['MAIL_DEFAULT_SENDER'] = 'help.scientral@gmail.com'
     mail.init_app(app)
     configure_logging(app)
